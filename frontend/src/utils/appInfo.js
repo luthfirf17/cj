fc5f0fe446c0b1ec430bea11c7fcc3fe6eb7ffa3
luthfirf17 <luthfirf17@gmail.com>
2025-11-12 06:10:11 +0700

    feat: Add app info module with copyright information

---
+++ frontend/src/utils/appInfo.js
@@ -0,0 +1,40 @@
+/**
+ * CatatJasamu - Sistem Manajemen Bisnis Modern
+ * 
+ * @author Cep Luthfi Rizky Fauzi
+ * @email catatjasamu@gmail.com
+ * @github https://github.com/luthfirf17
+ * @license MIT
+ * @copyright Copyright (c) 2025 Cep Luthfi Rizky Fauzi
+ * 
+ * Location:
+ * Kampung Cibarani, Desa Maggungjaya
+ * Kecamatan Rajapolah, Kabupaten Tasikmalaya
+ * Jawa Barat, Indonesia
+ * 
+ * Description:
+ * Aplikasi web lengkap untuk mengelola bisnis jasa event organizer,
+ * meliputi manajemen klien, layanan, booking, pembayaran, dan laporan keuangan.
+ * 
+ * Technology Stack:
+ * - Frontend: React 18, Vite, Tailwind CSS
+ * - Backend: Node.js, Express, PostgreSQL
+ * - DevOps: Docker, Docker Compose
+ */
+
+export const APP_INFO = {
+  name: 'CatatJasamu',
+  version: '1.0.0',
+  description: 'Sistem Manajemen Bisnis Modern untuk Event Organizer',
+  author: {
+    name: 'Cep Luthfi Rizky Fauzi',
+    email: 'catatjasamu@gmail.com',
+    github: 'https://github.com/luthfirf17',
+    location: 'Tasikmalaya, Jawa Barat, Indonesia'
+  },
+  repository: 'https://github.com/luthfirf17/cj',
+  license: 'MIT',
+  copyright: `Copyright © ${new Date().getFullYear()} Cep Luthfi Rizky Fauzi. All rights reserved.`
+};
+
+export default APP_INFO;
